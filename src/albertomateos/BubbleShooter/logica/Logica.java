3f5b40009185559b0e64847f773ea0a0695c1fab
PC07 <PC07@DESKTOP-U7KJ0D0>
2019-02-18 12:59:34 +0100

    Merge origin/master
    Conflicts:
    	src/bubbleshooter/BubbleShooter.java

--- src/albertomateos/BubbleShooter/logica/Logica.java
+++ src/albertomateos/BubbleShooter/logica/Logica.java
@@ -5,10 +5,49 @@
  */
 package albertomateos.BubbleShooter.logica;
 
+import bubbleshooter.BubbleShooter;
+
 /**
  *
  * @author PC07
  */
 public class Logica {
     
+    public static void main(String[] args) {
+        BubbleShooter bubbleshooter= new BubbleShooter();
+        bubbleshooter.mostrarTablero();
+        bubbleshooter.ponerBola();
+         System.out.println("");
+         
+         bubbleshooter.ponerbola(1,1,1,1,1,1,1,1,1);
+         bubbleshooter.ponerbola(1,0,1,0,1,0,1,0,1);
+         bubbleshooter.ponerbola(1,1,1,0,0,0,1,1,1);
+         bubbleshooter.mostrarTablero();
+    }
+    int[][] matrizTablero = new int[9][9];
+    public void mostrarTablero() {
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                System.out.print(matrizTablero[i][j]);
+        for(int y=0; y<9; y++) {
+            for(int x=0; x<9; x++) {
+                System.out.print(matrizTablero[x][y]);
+                }
+            System.out.println();
+            }
+        }
+    }
 }
+     public void ponerFicha() {
+        matrizTablero[9][9] = 1;
+        matrizTablero[9][8] = 2;
+     }
+        
+     public void ponerFicha(int x, int y, int jugador) {
+        matrizTablero [x][y] = jugador;
+     }
+     }  
+
+
+     
+
